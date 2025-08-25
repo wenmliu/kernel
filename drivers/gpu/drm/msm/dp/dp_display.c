03351e9eeeb27c86e2cd61abe67e0101a81588fa
Yongxing Mou <yongxing.mou@oss.qualcomm.com>
2025-08-25 22:16:24 +0800

    FROMLIST: drm/msm/dp: Add MST stream support for SA8775P DP controller 0 and 1
    This change enables SA8775P support for Multi-Stream Transport (MST),
    allowing each controller to handle up to two or four DisplayPort streams.
    As all necessary code for MST support was already implemented in the
    previous series of patches.
    
    Signed-off-by: Yongxing Mou <yongxing.mou@oss.qualcomm.com>
    Link: https://lore.kernel.org/all/20250825-msm-dp-mst-v3-38-01faacfcdedd@oss.qualcomm.com/

--- drivers/gpu/drm/msm/dp/dp_display.c
+++ drivers/gpu/drm/msm/dp/dp_display.c
@@ -115,8 +115,10 @@ static const struct msm_dp_desc msm_dp_desc_glymur[] = {
 };
 
 static const struct msm_dp_desc msm_dp_desc_sa8775p[] = {
-	{ .io_start = 0x0af54000, .id = MSM_DP_CONTROLLER_0, .wide_bus_supported = true },
-	{ .io_start = 0x0af5c000, .id = MSM_DP_CONTROLLER_1, .wide_bus_supported = true },
+	{ .io_start = 0x0af54000, .id = MSM_DP_CONTROLLER_0, .wide_bus_supported = true,
+	  .mst_streams = 4},
+	{ .io_start = 0x0af5c000, .id = MSM_DP_CONTROLLER_1, .wide_bus_supported = true,
+	  .mst_streams = 2},
 	{ .io_start = 0x22154000, .id = MSM_DP_CONTROLLER_0, .wide_bus_supported = true },
 	{ .io_start = 0x2215c000, .id = MSM_DP_CONTROLLER_1, .wide_bus_supported = true },
 	{}
